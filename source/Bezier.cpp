5dae979b69f9bc3d2f1b5e7c90d5e2eb1be8f887
EmilianoCervantes <ovman96@hotmail.com>
2018-04-20 18:02:38 -0500

    Dibujando Bezier

--- source/Bezier.cpp
+++ source/Bezier.cpp
@@ -4,7 +4,7 @@ Bezier::Bezier(int d, Point* ctrl)
 {
 	degree = d;
 	ctrlPoints = new Point[degree];
-	for (int i = 0; i < degree; i++)
+	for (int i = 0; i < degree+1; i++)
 	{
 		ctrlPoints[i].x = ctrl[i].x;
 		ctrlPoints[i].y = ctrl[i].y;
@@ -16,17 +16,28 @@ Bezier::Bezier(int d, Point* ctrl)
 	}
 	coefficients = new float[degree];
 	//Inicializar coeficientes para usar después.
+	for (int i = 0; i < degree; i++)
+	{
+		coefficients[i] = binomial(degree, i);
+	}
+
+	evalBezier(0.9f);
 }
 
 Point* Bezier::evalBezier(float t)
 {
-	Point* ps = new Point[2];
+	Point* ps = new Point[1];
+	ps[0].r = 0;
+	ps[0].g = 0;
+	ps[0].b = 1.0;
 	for (int i = 0; i < degree; i++)
 	{
-		ps[0].x += coefficients[i] * powf(1.0f - t, n - i) * powf(t, i) * ctrlPoints[i].x;
-		ps[0].y += coefficients[i] * powf(1.0f - t, n - i) * powf(t, i) * ctrlPoints[i].y;
-		ps[0].z += coefficients[i] * powf(1.0f - t, n - i) * powf(t, i) * ctrlPoints[i].z;
+		ps[0].x += coefficients[i] * powf(1.0f - t, degree - i) * powf(t, i) * ctrlPoints[i].x;
+		ps[0].y += coefficients[i] * powf(1.0f - t, degree - i) * powf(t, i) * ctrlPoints[i].y;
+		ps[0].z += coefficients[i] * powf(1.0f - t, degree - i) * powf(t, i) * ctrlPoints[i].z;
 	}
+
+	return ps;
 }
 
 int Bezier::factorial(int x)
@@ -38,6 +49,18 @@ int Bezier::factorial(int x)
 float Bezier::binomial(int n, int i)
 {
 	return (float)factorial(n) / (factorial(i)*factorial(n - i));
+}
+
+void Bezier::draw()
+{
+	glPushMatrix();
+	{
+		for (int i = 0; i < degree; i++)
+		{
+			ctrlPoints[i].draw();
+		}
+	}
+	glPopMatrix();
 }
 
 
